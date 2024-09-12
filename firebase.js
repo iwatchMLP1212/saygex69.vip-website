2669ecd4be2693759bfc21a9362763af79c0ed8c
snuwfl <duyminht651@gmail.com>
2024-09-12 20:51:33 +0700

    Added firebase-hosting.yml & firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBcxIK75MI5dveu7bKC3NftH6qerrFHtwg",
+  authDomain: "saygex69.firebaseapp.com",
+  projectId: "saygex69",
+  storageBucket: "saygex69.appspot.com",
+  messagingSenderId: "71095943069",
+  appId: "1:71095943069:web:67ec16db02ae9cf5d0b974"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
